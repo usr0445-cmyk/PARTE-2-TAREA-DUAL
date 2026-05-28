5fcbf6865cf9da3328437bb3baefe0e44a2b426a
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 17:27:53 +0100

    Creamos la estructura principal del App, haciendo un menu para elegir al investigador, importando el Scanner

--- src/App.java
+++ src/App.java
@@ -1,3 +1,15 @@
+
+import java.util.Scanner;
+
 public class App {
-    
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selecciona el tipo de investigador:");
+        System.out.println("1. Biologo");
+        System.out.println("2. Quimico");
+        System.out.print("3. Matematico");
+
+        
+    }
 }
