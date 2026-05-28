db6a87841f58a31f1e7654d2fd69828fabf0bd84
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 16:14:27 +0100

    Hacemos un private ArrayList para guardar los especimenes que el biologo esta estudiando y despues creamos el constructor para inicializar los atributos de la clase biologo

--- src/net/salesianoslacuesta/Biologo/biologo.java
+++ src/net/salesianoslacuesta/Biologo/biologo.java
@@ -2,6 +2,12 @@ package net.salesianoslacuesta.Biologo;
 
 import java.util.ArrayList;
 
-public class biologo extends investigador{
+public class biologo extends investigador {
     
+    private ArrayList<String> especimenes;
+
+    public biologo(String nombre, double sueldo) {
+        super(nombre, "Biologia", sueldo);
+        this.especimenes = new ArrayList<>();
+    }
 }
