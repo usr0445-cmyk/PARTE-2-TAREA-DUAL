70c8ae3bf108c51b65a87588f251b5a73f21b497
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 12:48:41 +0100

    Creamos este constructor para poder inicializar los atributos de la clase matematico que hereda de investigador y ponemos el metodo abstracto trabajar para mostrar un mensaje de la matriz

--- src/net/salesianoslacuesta/Matematico/matematico.java
+++ src/net/salesianoslacuesta/Matematico/matematico.java
@@ -4,5 +4,14 @@ public abstract class matematico extends investigador {
 
     private int[][] resultadosActuales; 
 
+    public matematico(String nombre, int[][] matrizInicial, double sueldo) {
+        super(nombre, "Matematicas", sueldo);
+        this.resultadosActuales = matrizInicial; 
+    }
+
+    @Override
+    public void trabajar() {
+        System.out.println(" Esta trabajando en una matriz 3x3");
+    }
 
 }
