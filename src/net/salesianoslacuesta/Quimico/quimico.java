6b846cf9e7a885c9e723cdda2ba7b38a701092ab
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 16:51:40 +0100

    Creamos este constructor para iniciar los atributos de quimico, y ponemos public void para implementar el metodo de la interfaz

--- src/net/salesianoslacuesta/Quimico/quimico.java
+++ src/net/salesianoslacuesta/Quimico/quimico.java
@@ -4,5 +4,15 @@ import java.util.ArrayList;
 public class quimico extends investigador implements trabajoDual {
 
     private ArrayList<String> elementos;
+
+    // Creamos este constructor para iniciar los atributos de quimico
+    public quimico(String nombre, double sueldo) {
+        super(nombre, "Quimica", sueldo);
+        this.elementos = new ArrayList<>();
+    }
+
+    public void agregarElemento(String elemento) {
+        elementos.add(elemento);
+    }
     
 }
