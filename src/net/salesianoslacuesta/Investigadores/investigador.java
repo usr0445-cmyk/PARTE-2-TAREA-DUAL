7d8ee3669ed727d91a66edc9733ae535e2c48c49
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 12:40:21 +0100

    Añado un par de comentarios

--- src/net/salesianoslacuesta/Investigadores/investigador.java
+++ src/net/salesianoslacuesta/Investigadores/investigador.java
@@ -2,11 +2,12 @@ package net.salesianoslacuesta.Investigadores;
 
 public abstract class investigador {
     
+    //Aqui creamos los atributos de la clase investigador
     protected String nombre;
     protected String especialidad;
     protected double sueldo;
 
-   
+   //Creamos este constructor para poder inicializar los atributos de la clase investigador
     public investigador(String nombre, String especialidad, double sueldo) {
         this.nombre = nombre;
         this.especialidad = especialidad;
@@ -14,6 +15,7 @@ public abstract class investigador {
     }
 
 
+    //Aqui creamos el metodo identificacion para mostrar la informacion
     public void identificacion() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Especialidad: " + especialidad);
@@ -21,6 +23,7 @@ public abstract class investigador {
     }
 
 
+    //Aqui ponemos dos metodos para hacer calculos, uno para sumar y otro para restar
     public int hacerCalculoSuma(int a, int b) {
         return a + b;
     }
@@ -29,7 +32,7 @@ public abstract class investigador {
         return a - b;
     }
 
-    public abstract String estadoDelSueldo();
+    //Y dos metodos abstractos para que las clases hijas los implementen, uno para investigar y otro para trabajar
     public abstract void trabajar();
 
 
