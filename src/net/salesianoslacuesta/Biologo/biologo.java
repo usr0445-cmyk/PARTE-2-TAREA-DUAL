f4e712a50792c9044071a48cd93cc3676624a3b9
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 16:29:17 +0100

    Override para implementar el metodo trabajar en este caso el biologo va a ordenar los especimenes por su longitud y hacemos bucle para ordenar los especimenes por longitud de su nombre

--- src/net/salesianoslacuesta/Biologo/biologo.java
+++ src/net/salesianoslacuesta/Biologo/biologo.java
@@ -2,6 +2,8 @@ package net.salesianoslacuesta.Biologo;
 
 import java.util.ArrayList;
 
+import net.salesianoslacuesta.Investigadores.investigador;
+
 public class biologo extends investigador {
     
     private ArrayList<String> especimenes;
@@ -21,6 +23,30 @@ public class biologo extends investigador {
             return "El biologo solicita aumento de sueldo";
         } else {
             return "El sueldo del biologo es correcto";
+        }
+    }
+
+    @Override
+    public void trabajar(){
+        if (especimenes.isEmpty()) {
+            System.out.println("El biologo no tiene especimenes para estudiar");
+            return;
+    
+        }
+
+        for (int i = 0; i < especimenes.size() -1; i++) {
+            for (int j = 0; j < especimenes.size() - i - 1; j++) {
+                
+                String actual = especimenes.get(j);
+                String siguiente = especimenes.get(j + 1);
+                
+                if (actual.length() > siguiente.length()) {
+
+                    //Intercambiamos los especimenes
+                    especimenes.set(j, siguiente);
+                    especimenes.set(j + 1, actual);
+                }
+            }
         }
     }
 
