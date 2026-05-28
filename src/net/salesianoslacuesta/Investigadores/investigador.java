179d818dd92f2e0d2d64786f6bdb39a6fdb2e1b0
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 10:58:27 +0100

    He añadido los atributos necesarios para la clase investigador y he puesto el constructor para los objetos

---
+++ src/net/salesianoslacuesta/Investigadores/investigador.java
@@ -0,0 +1,17 @@
+package net.salesianoslacuesta.Investigadores;
+
+public abstract class investigador {
+    
+    protected String nombre;
+    protected String especialidad;
+    protected double sueldo;
+
+   
+    public investigador(String nombre, String especialidad, double sueldo) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.sueldo = sueldo;
+    }
+
+
+}
