0120387d84ff990357fa1e52ac259eb014b0cb8f
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-28 13:21:19 +0100

    Añadimos un par de comentarios

--- src/net/salesianoslacuesta/Matematico/matematico.java
+++ src/net/salesianoslacuesta/Matematico/matematico.java
@@ -4,33 +4,40 @@ import net.salesianoslacuesta.Investigadores.investigador;
 
 public abstract class matematico extends investigador {
 
+    //Aqui creamos un atributo para guardar los resultados actuales de la matriz
     private int[][] resultadosActuales; 
 
+    //Creamos este constructor para inicializar los atributos de la clase matematico
     public matematico(String nombre, int[][] matrizInicial, double sueldo) {
         super(nombre, "Matematicas", sueldo);
         this.resultadosActuales = matrizInicial; 
     }
 
+    //Override para mostrar la informacion del matematico
     @Override
     public void trabajar() {
         System.out.println(" Esta trabajando en una matriz 3x3");
     }
 
+    //Aqui creamos dos metodos para trabajar con la matriz, uno para multiplicar por un factor y otro para sumar otra matriz
     public void trabajar(int factor) {
         for (int i = 0; i < resultadosActuales.length; i++) {
             for (int j = 0; j < resultadosActuales[i].length; j++) {
+                // Multiplicamos cada elemento de la matriz por el factor
                 resultadosActuales[i][j] *= factor;
             }
         }
         System.out.println("La matriz ha sido multiplicada por el factor: " + factor);
     } 
 
+    //Metodo para sumar otra matriz a la matriz actual comprobando que ambas matrices sean 3x3
     public void trabajar(int otraMatriz[][] ) {
         if (otraMatriz.length != 3 || otraMatriz[0].length != 3) {
             System.out.println("La matriz recibida no es 3x3");
             return;
     }
 
+    // Sumamos cada elemento de la otra matriz a la matriz actual
     for (int i = 0; i < resultadosActuales.length; i++) {
         for (int j = 0; j < resultadosActuales[i].length; j++) {
             resultadosActuales[i][j] += otraMatriz[i][j];
@@ -39,7 +46,8 @@ public abstract class matematico extends investigador {
     System.out.println("Se han sumado las matrices correctamente");
 }
 
-//Aqui por ultimo ponemos override para el metodo estado del sueldo, para que el matematico pueda solicitar un aumento de sueldo si es necesario
+//Por ultimo, ponemos un metodo para mostrar el estado del sueldo del matematico
+
     @Override
     public String estadoDelSueldo() {
     if (sueldo < 1800) {
