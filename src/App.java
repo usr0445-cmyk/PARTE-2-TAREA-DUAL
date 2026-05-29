54a85b2bb877c474c390522f4382b43577a3891c
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-29 11:40:24 +0100

    Aqui ponemos 3 if para crear el investigador segun la opcion elegida por el usuario, y en el caso del matematico, le damos una matriz inicial de 3x3, falta arreglar un par de fallos

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import net.salesianoslacuesta.Biologo.biologo;
 import net.salesianoslacuesta.Investigadores.investigador;
 import net.salesianoslacuesta.Quimico.quimico;
+import net.salesianoslacuesta.Matematico.matematico;
 
 
 public class App {
@@ -17,8 +18,15 @@ public class App {
         int opcion = scanner.nextInt();
         scanner.nextLine();
 
-        //
-        investigador investigador = null;
+        investigador investigador = null;   
+
+        if (opcion == 1) {
+            investigador = new biologo("Biologo", 1500);
+        } else if (opcion == 2) {
+            investigador = new quimico("Quimico", 1600);
+        } else if (opcion == 3) {
+            investigador = new matematico("Matematico", 1700, matriz);
+        }
 
 
     }
